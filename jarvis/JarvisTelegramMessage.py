2d59733bf55c340ccfac53fd0b611b836f5885c4
FiksUpped <niksd@tut.by>
2021-09-20 18:00:18 +0300

    f

--- jarvis/JarvisTelegramMessage.py
+++ jarvis/JarvisTelegramMessage.py
@@ -3,10 +3,10 @@ from telethon import TelegramClient
 api_hash = '00765fc90e559b58fa869ed7fd9bbf1f'
 api_id = 1917091
 
-username= "@dejkaa"
+username= "@"
 
 client = TelegramClient('session_name', api_id, api_hash)
 
-client.start(password="AquAsonic3346378sb")
+client.start(password="")
 
 client.send_message('me', 'Hello to myself!')
